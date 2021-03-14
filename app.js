c240876ecb1f1116dc1dd502d77cff47f4627118
Abdoulaye Diallo <dialloabdoulaye71@gmail.com>
2021-03-14 02:59:31 +0000

    local -> online

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 // Database
 var cluster = 'mongodb+srv://celafinde:69305565@cluster0.wmifd.mongodb.net/sabus-app'
 var mongoDB = "mongodb://localhost/sabus-app";
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(cluster, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
